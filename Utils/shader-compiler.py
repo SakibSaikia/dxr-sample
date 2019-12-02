bff38567bea32c6ae2f42d4ff88bb780f9a8984a
SakibSaikia <sakib.r.saikia@gmail.com>
2019-12-01 23:37:56 -0800

    Missed file in last commit

--- Utils/shader-compiler.py
+++ Utils/shader-compiler.py
@@ -4,7 +4,7 @@ dxc = "..\\libs\\dxc\\bin\\dxc.exe"
 shader_dir = "..\\Src\\"
 output_dir = "..\\Src\\CompiledShaders\\"
 profile = "lib_6_3"
-additional_args = " -Zpr "
+common_args = " -Zpr "
 
 raygen_shader_list = {
 	"Raygen.hlsl" : ".rgs" 
@@ -17,31 +17,37 @@ mtl_shader_list = {
 
 def build_raygen_shaders():
 	bytecode_prefix = "raygen"
+	args = common_args
 	for shader_file, bytecode_annotation in raygen_shader_list.items():
 		shader_path = shader_dir + shader_file
 		compiled_bytecode_path = output_dir + bytecode_prefix + bytecode_annotation + ".cso"
-		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + additional_args + shader_path)
+		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + args + shader_path)
 
 def build_default_shaders():
 	bytecode_prefix = "mtl_default"
+	args = common_args
 	for shader_file, bytecode_annotation in mtl_shader_list.items():
 		shader_path = shader_dir + shader_file
 		compiled_bytecode_path = output_dir + bytecode_prefix + bytecode_annotation + ".cso"
-		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + additional_args + shader_path)
+		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + args + shader_path)
 
 def build_masked_shaders():
 	bytecode_prefix = "mtl_masked"
+	additional_args = "-D\"MASKED\" "
+	args = common_args + additional_args
 	for shader_file, bytecode_annotation in mtl_shader_list.items():
 		shader_path = shader_dir + shader_file
 		compiled_bytecode_path = output_dir + bytecode_prefix + bytecode_annotation + ".cso"
-		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + additional_args + shader_path)
+		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + args + shader_path)
 
 def build_untextured_shaders():
 	bytecode_prefix = "mtl_untextured"
+	additional_args = "-D\"UNTEXTURED\" "
+	args = common_args + additional_args
 	for shader_file, bytecode_annotation in mtl_shader_list.items():
 		shader_path = shader_dir + shader_file
 		compiled_bytecode_path = output_dir + bytecode_prefix + bytecode_annotation + ".cso"
-		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + additional_args + shader_path)
+		os.system(dxc + " -T" + profile + " -Fo" + compiled_bytecode_path + args + shader_path)
 
 
 build_raygen_shaders()
